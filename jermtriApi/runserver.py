e9c0c54197c4e5f418ccfbe7cc298ee7f910b5fa
Armand <armand.dusart@octomine.com>
2022-07-06 20:48:04 +0200

    python

--- jermtriApi/runserver.py
+++ jermtriApi/runserver.py
@@ -4,7 +4,6 @@ This script runs the jermtriApi application using a development server.
 
 from os import environ
 from flask import Flask, jsonify, request 
-from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
@@ -16,11 +15,19 @@ api = Api(app)
 CORS(app, resources={r"/*":{'origins':"*"}})
 # CORS(app, resources={r'/*':{'origins': 'http://localhost:8080',"allow_headers": "Access-Control-Allow-Origin"}})
 
+#! Controller
 class TestApi(Resource):
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(TestApi, '/')
+class getProductById(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(getProductById, '/test')
+
+#! Running
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
